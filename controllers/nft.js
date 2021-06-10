dd17eadd9e9979c6568b93517f3808646cacf1ec
MikeHibbert <hibbert.michael@gmail.com>
2021-06-10 08:50:53 +0100

    added proxy

--- controllers/nft.js
+++ controllers/nft.js
@@ -4,12 +4,12 @@ function getMediaElement(nft) {
   const contentType = nft['Content-Type'];
 
   if(nft['Content-Type'].indexOf('image') != -1) {
-    return `<img src="/image/${nft.id}" title="${nft.name}" class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>`;
+    return `<img src="/nfts/image/${nft.id}" title="${nft.name}" class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>`;
     
   }
 
   if(nft['Content-Type'].indexOf('video') != -1) {
-    let poster = "/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE";
+    let poster = "/nfts/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE";
         
     return `<div class="embed-responsive embed-responsive-16by9" style="object-fit: cover; height: auto; width: auto;" >
             <video preload="auto" controls poster="${poster}">
@@ -20,14 +20,14 @@ function getMediaElement(nft) {
 
   if(nft['Content-Type'].indexOf('audio') != -1) {
     return `<div style="text-align: center;">
-            <img src="/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE" title={initialStateTag.name} class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>
+            <img src="/nfts/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE" title={initialStateTag.name} class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>
             <audio preload="auto" controls autoplay >
                 <source src="/image/${nft.id}" type=${contentType} />
             </audio>
         </div>`;
   }
 
-  return `<img src="/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE" title="${nft.name}" class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>`
+  return `<img src="/nfts/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE" title="${nft.name}" class="img-fluid" style="object-fit: cover; height: auto; width: auto;"></img>`
 }
 
 exports.detail = async function(req, res, next) {
@@ -86,7 +86,7 @@ exports.detail = async function(req, res, next) {
 
         const detail_url = `https://www.evermoredata.store/ntfs/nft-detail/${node.id}`;
 
-        let image_url = "/nfts/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE";
+        let image_url = "/image/c1mNDCo3Mh1PdimUr5OEYyVdbgOowXV2Ct_JN5irnRE";
 
         if(node['Content-Type'].indexOf('image') != -1) {
           image_url = `/image/${node.id}`;
